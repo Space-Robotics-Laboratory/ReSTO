7e09088a184f2d1910e139ce5befa08dcea1bdb2
MasazumiImai <imai.masazumi.p2@dc.tohoku.ac.jp>
2026-04-02 00:27:56 +0900

    Add cost for momentum

--- mlivr_control/src/wbc_solver.cpp
+++ mlivr_control/src/wbc_solver.cpp
@@ -185,14 +185,14 @@ std::shared_ptr<crocoddyl::ActionModelAbstract> WbcSolver::createActionModel(
     "control_reg", std::make_shared<crocoddyl::CostModelResidual>(state_, u_residual),
     params_.weights.control_reg);
 
-  // // ベースと全関節の動きによって生じる空間全体の運動量(h = [linear, angular])を計算し、
-  // // それがゼロ(Force::Zero)から変動しないようにペナルティを与える
-  // auto momentum_residual = std::make_shared<crocoddyl::ResidualModelCentroidalMomentum>(
-  //   state_, Eigen::VectorXd::Zero(6), actuation_->get_nu());
+  // ベースと全関節の動きによって生じる空間全体の運動量(h = [linear, angular])を計算し、
+  // それがゼロ(Force::Zero)から変動しないようにペナルティを与える
+  auto momentum_residual = std::make_shared<crocoddyl::ResidualModelCentroidalMomentum>(
+    state_, Eigen::VectorXd::Zero(6), actuation_->get_nu());
 
-  // costs->addCost(
-  //   "momentum_reg", std::make_shared<crocoddyl::CostModelResidual>(state_, momentum_residual),
-  //   1e3);  // 重みで調整
+  costs->addCost(
+    "momentum_reg", std::make_shared<crocoddyl::CostModelResidual>(state_, momentum_residual),
+    1e3);  // 重みで調整
 
   // Differential-Algebraic Model (DAM)
   auto dmodel = std::make_shared<crocoddyl::DifferentialActionModelContactFwdDynamics>(
