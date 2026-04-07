05fc696eeaba77265cb5fd2d6818008b3dfff1d8
MasazumiImai <imai.masazumi.p2@dc.tohoku.ac.jp>
2026-04-08 01:42:33 +0900

    Change state reg cost

--- mlivr_control/src/wb_control.cpp
+++ mlivr_control/src/wb_control.cpp
@@ -38,11 +38,11 @@ WBControl::WBControl(const rclcpp::NodeOptions & options) : Node("wb_control", o
   WbcSolverParams params;
   params.solver.horizon_steps = this->declare_parameter<int>("solver.horizon_steps", 100);
   params.solver.dt = this->declare_parameter<double>("solver.dt", 0.01);
-  params.weights.swing_goal = this->declare_parameter<double>("solver.swing_goal", 1e4);
-  params.weights.state_reg = this->declare_parameter<double>("solver.state_reg", 1e-1);
-  params.weights.control_reg = this->declare_parameter<double>("solver.control_reg", 1e-4);
-  params.weights.state_limits = this->declare_parameter<double>("solver.state_limits", 1000.0);
-  params.weights.control_limits = this->declare_parameter<double>("solver.control_limits", 1000.0);
+  params.weights.swing_goal = this->declare_parameter<double>("weights.swing_goal");
+  params.weights.state_reg = this->declare_parameter<double>("weights.state_reg");
+  params.weights.control_reg = this->declare_parameter<double>("weights.control_reg");
+  params.weights.state_limits = this->declare_parameter<double>("weights.state_limits");
+  params.weights.control_limits = this->declare_parameter<double>("weights.control_limits");
 
   std::string urdf_path =
     ament_index_cpp::get_package_share_directory("mlivr_description") + "/urdf/mlivr.urdf";
