a3082b67efdf5c327725698fe5c0b52ada92cc1a
MasazumiImai <imai.masazumi.p2@dc.tohoku.ac.jp>
2026-04-23 23:23:09 +0900

    Update init gait

--- mlivr_sim/src/mujoco_engine.cpp
+++ mlivr_sim/src/mujoco_engine.cpp
@@ -59,7 +59,7 @@ MujocoEngine::MujocoEngine(const std::string & xml_path, bool extract_keyframe)
       // d_->qpos[5] = 0.0;   // qy
       // d_->qpos[6] = 0.0;   // qz
       d_->qpos[0] = 0.52;            // x
-      d_->qpos[1] = 0.0;             // y
+      d_->qpos[1] = -0.25;           // y
       d_->qpos[2] = 1.0;             // z
       d_->qpos[3] = 1.0 / sqrt(2);   // qw
       d_->qpos[4] = 0.0;             // qx
