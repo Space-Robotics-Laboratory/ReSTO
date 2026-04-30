e3f4b8467af5d9cb6ba029bf34878cf203d965b0
MasazumiImai <imai.masazumi.p2@dc.tohoku.ac.jp>
2026-04-30 23:58:21 +0900

    Move dir

--- tools/rosbag/plotEEForceTorque.m
+++ tools/rosbag/plotEEForceTorque.m
@@ -18,7 +18,8 @@ clc; clear; close all;
 
 % type = "to";  % kj/gj/lrst/ramp-pmd/ramp-fmd/to
 % csv_file = "csv" + filesep + type + "_" + "rosbag2_2026_04_22-04_44_11" + ".csv";
-csv_file = "csv" + filesep + "rosbag2_2026_04_30-20_42_39" + ".csv";
+case_id = 1;
+csv_file = "case-" + case_id + filesep + "csv" + filesep + "rosbag2_2026_04_30-22_09_24" + ".csv";
 
 save_fig = false;  % true/false
 
