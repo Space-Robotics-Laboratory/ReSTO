a85bf8d9667e6dc4c3e04f95538fca0f0c32c079
MasazumiImai <imai.masazumi.p2@dc.tohoku.ac.jp>
2026-07-09 09:38:03 +0900

    Get data for resto err 10%

--- tools/rosbag/case-3/analyzeCase3Data.m
+++ tools/rosbag/case-3/analyzeCase3Data.m
@@ -20,7 +20,7 @@ case_id = 3;
 
 % Select one method and one uncertainty level.
 % Examples: "baseline", "gjm", "ramp-pmd", "ramp-fmd", "resto"
-method_name = "ramp-fmd";
+method_name = "resto";
 
 % Uncertainty level in percent. Examples: 5, 10, 20
 uncertainty_percent = 10;
